1524d2e5cefd4183057925a55d1d871c88d0b147
Marcelo Fritz <marceloyo85@gmail.com>
2014-06-24 09:29:04 -0300

    Removido areas comentadas do main

--- com.senac.sistemaissues/src/application/aplicacao.java
+++ com.senac.sistemaissues/src/application/aplicacao.java
@@ -9,18 +9,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 
-//		Lista1 teste = new Lista1();
-//		Lista2 teste2 = new Lista2();
-// 		
-//		System.out.println("Lista 1 ____________________");
-//		teste.adicionaNoComeco("Nome");
-//		teste.adicionaNoComeco("Nome2");
-//		System.out.println(teste);
-//		System.out.println(teste.contem("Nome"));
-//		
-//		
-//		System.out.println("Lista 2 ____________________");
-//		teste2.exibirLista();
+
 		
 		Controller vai = new Controller();
 		
